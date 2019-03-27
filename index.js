fd4871b98168e95513a1cf0cbbb895e8f5165f15
timqian <timqian92@qq.com>
2019-03-27 17:26:42 +0800

    auto hide burger

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
+
+  
     var burger = document.querySelector('.burger');
     var menu = document.querySelector('#'+burger.dataset.target);
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+
+    document.addEventListener('click', function() {
+      burger.classList.remove('is-active');
+      menu.classList.remove('is-active');
+    });
+
+    burger.addEventListener('click', function(event) {
+      event.stopPropagation();
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
     });
 
     // typed.js
